9b7bd04335f718d5e4002251fa6d5a627df7a5dd
emilywu1301 <emilywu1301@gmail.com>
2018-08-15 13:38:46 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,3 +1,15 @@
+# require_relative 'config/environment'
+
+# class App < Sinatra::Base
+
+#   get '/food_form' do
+#     erb :food_form
+#   end
+
+#   # Add your post route and action below
+
+# end
+
 require_relative 'config/environment'
 
 class App < Sinatra::Base
@@ -7,5 +19,6 @@ class App < Sinatra::Base
   end
 
   # Add your post route and action below
-
+post '/food' do
+  "my name is #{params[:name]}, and i love #{params[:favorite_food]}
 end
